08543c0294dcec5d726e36881e1a1de44521c025
Saad Shamim <saadshamim01@gmail.com>
2021-09-06 22:43:10 +0400

    Created first react app

---
+++ docs/react/first-react-app/src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1> This is first line</h1>
+    <h2> This is my second second</h2>
+  </div>
+  ,document.getElementById("root"));
+
