70176ad9505742ceb3063c389bfb545b553a2c5d
Saad Shamim <saadshamim01@gmail.com>
2021-09-17 15:40:55 +0400

    Added subnet & security group

--- docs/terraform/apache-server-using-terraform/main.tf
+++ docs/terraform/apache-server-using-terraform/main.tf
@@ -22,6 +22,16 @@ resource "aws_vpc" "prod-vpc" {
   }
 }
 
+# 2. Create internet gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+}
+
+# 3. Create Custom Route Table
+
+
 resource "aws_route_table" "prod-route-table" {
   vpc_id = aws_vpc.prod-vpc.id
 
@@ -36,5 +46,70 @@ resource "aws_route_table" "prod-route-table" {
     }
   tags = {
     Name = "Prod"
+  }
+}
+
+# 4. Create a subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# 5. Associate subnet with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create security group to allow port 22, 80, 443
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+      description      = "HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  ingress {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["0.0.0.0/0"]
+    }
+  ingress {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "allow_web"
   }
 }
