1f1bf49cea0eec6e336ec4dc92e425b59b265782
Saad Shamim <saadshamim01@gmail.com>
2021-09-17 15:38:59 +0400

    Added route table

--- docs/terraform/apache-server-using-terraform/main.tf
+++ docs/terraform/apache-server-using-terraform/main.tf
@@ -21,3 +21,20 @@ resource "aws_vpc" "prod-vpc" {
     Name = "production"
   }
 }
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+      # Routing all the traffic to gateway
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
+  route {
+      ipv6_cidr_block        = "::/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
+  tags = {
+    Name = "Prod"
+  }
+}
