2ef2c4524eae85e0a698d09732a95ef655e419a1
Saad Shamim <saadshamim01@gmail.com>
2021-09-19 23:23:00 +0400

    Added DynamoDb table

--- docs/terraform/dynamodb-using-terraform/main.tf
+++ docs/terraform/dynamodb-using-terraform/main.tf
@@ -12,3 +12,26 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+resource "aws_dynamodb_table" "student-stable" {
+  name           = "Student-Table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "StudentId"
+  range_key      = "Name"
+
+  attribute {
+    name = "StudentId"
+    type = "N"
+  }
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
