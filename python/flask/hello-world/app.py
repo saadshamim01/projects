4dfa383de1839adfd322f86962ef2b57b5393eab
Saad Shamim <saadshamim01@gmail.com>
2021-09-24 20:00:11 +0400

    Added new endpoint

--- python/flask/hello-world/app.py
+++ python/flask/hello-world/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -6,12 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify('Hello World!'), 200
 
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the Planetary API.'
+    return jsonify(message="Simple Life!")
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message="Not found"), 404
+
+@app.route('/parameters')
+def parameters():
+    name= request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message = "Sorry " + name + " you are not old enough"), 401
+    else:
+        return jsonify(message = "Welcome " + name + " you are old enough"), 200
+
 
 
 if __name__ == '__main__':
