57706916ea6c6ecd052bd2b57f0e77c1e4925574
Saad Shamim <saadshamim01@gmail.com>
2021-10-05 13:19:52 +0400

    Added progress-bar

---
+++ python/flask/progress-bar/basic-progress-bar.py
@@ -0,0 +1,6 @@
+from tqdm import tqdm
+import time
+
+
+for i in tqdm([1, 2, 3, 4, 5]):
+    time.sleep()
---
+++ python/flask/progress-bar/progress-bar.py
@@ -0,0 +1,10 @@
+from tkinter.ttk import Progressbar
+from tkinter import *
+
+w = Tk()
+w.geometry('450x250')
+
+width_of_window = 427
+height_of_window = 250
+screen_width = w.winfo_screenwidth()
+screen_height = w.winfo_screenheight()
