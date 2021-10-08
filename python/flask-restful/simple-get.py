b621a4d7100a5db9046401a128d13ce6d88ab1be
Saad Shamim <saadshamim01@gmail.com>
2021-10-08 22:55:40 +0400

    Added API

--- python/flask-restful/simple-get.py
+++ python/flask-restful/simple-get.py
@@ -8,7 +8,13 @@ class HelloWorld(Resource):
     def get(self):
         return {"Hello": "World"}
 
+class Hi(Resource):
+    def get(self):
+        return {"Hello": "saad!"}
+
 api.add_resource(HelloWorld, '/')
+
+api.add_resource(Hi, '/hi')
 
 if __name__ == "__main__":
     app.run(debug=True)
