940544de86c59ba379e4242074bc4f0190bcbbaf
Saad Shamim <saadshamim01@gmail.com>
2021-11-06 22:09:28 +0400

    Added COMP_LIFE

--- docs/react/reactproj/src/index.js
+++ docs/react/reactproj/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './form.jsx';
+import COMP_LIFE from './form.jsx';
 
 ReactDOM.render(
-    <Form />,
+    <COMP_LIFE />,
     document.getElementById('root')
 );
