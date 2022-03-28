8020a01d0679482d105f9ea28e8726c65b7b5b16
Saad Shamim <saadshamim01@gmail.com>
2022-03-28 19:22:22 +0400

    Updated urls

--- django/user_management/users/urls.py
+++ django/user_management/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, RegisterView, CustomLoginView
-
+from .views import home, RegisterView, profile
 
 urlpatterns=[
     path('', home, name='users-home'),
+    path('profile/', profile, name='profile'),
     path('register/', RegisterView.as_view(), name='users-register'),
 ]
