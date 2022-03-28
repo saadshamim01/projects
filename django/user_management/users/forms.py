f3e958151eb844c5fdf9de07f0da9df05cd002e1
Saad Shamim <saadshamim01@gmail.com>
2022-03-28 23:06:59 +0400

    Updated views and forms

--- django/user_management/users/forms.py
+++ django/user_management/users/forms.py
@@ -2,6 +2,37 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+from .models import Profile
+
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+    }))
+
+    email = forms.EmailField(max_length=100, required=True, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+    }))
+
+    class Meta:
+        model=User
+        fields=['username', 'email']
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={
+        'class': 'form-control-input'
+        }))
+    
+    bio = forms.CharField(max_length=200, required=True, widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 5,
+    }))
+
+    class Meta:
+        model=Profile
+        fields=['avatar', 'bio']
+
+
 
 class RegisterForm(UserCreationForm):
     
@@ -62,4 +93,3 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'remember_me']
-
--- django/user_management/users/views.py
+++ django/user_management/users/views.py
@@ -2,23 +2,48 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
 
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView, PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class CustomLoginView(View):
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name='user/change_password.html'
+    success_message="Successfully Changed Your Password"
+    success_url=reverse_lazy('users-home')
+    
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name='user/password_reset.html'
+    email_template_name='users/password_reset_email.html'
+    subject_template_name='users/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
+
+
+class CustomLoginView(LoginView):
     form_class = LoginForm
 
     def form_valid(self, form):
         remember_me = form.cleaned_data.get('remember_me')
 
         if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
             self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
             self.request.session.modified = True
 
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
         return super(CustomLoginView, self).form_valid(form)
-
 
 class RegisterView(View):
     form_class= RegisterForm
@@ -44,11 +69,32 @@ class RegisterView(View):
         return render(request, self.template_name, {'form': form})
 
     def dispatch(self, request, *args, **kwargs):
-
+    
         if request.user.is_authenticated:
-            redirect('/')
+            return redirect(to='/')
 
-        return super(RegisterView, request).dispatch(self, *args, **kwargs)
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+
 
 def home(request):
     return render(request, 'users/home.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+
+    else:
+        user_form=UpdateUserForm(instance=request.user)
+        profile_form=UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+    
