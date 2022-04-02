16974a5abb55084ed169719ed4ca6523fc8f9449
Saad Shamim <saadshamim01@gmail.com>
2022-04-02 21:05:11 +0400

    Switched to react-router-dom

--- react/amazon-clone/src/App.js
+++ react/amazon-clone/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={[<Checkout />]} />
+          <Route path="/" element={[<Home />]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
