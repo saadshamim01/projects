d78f3da2fc4309c8f45a811d0e52529dcdb91d96
Saad Shamim <saadshamim01@gmail.com>
2022-04-06 02:56:51 +0400

    Added CartScreen

--- django/ecommerce/frontend/src/App.js
+++ django/ecommerce/frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -14,7 +15,12 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
-            <Route path="product/:id/*" element={<ProductScreen />} />
+            <Route path="product/:id/" element={<ProductScreen />} />
+            <Route path="cart/:id?/" element={<CartScreen />} />
+            <Route path="cart/">
+              <Route path=":id" element={<CartScreen />} />
+              <Route path="" element={<CartScreen />} />
+            </Route>
           </Routes>
           {/* <HomeScreen /> */}
         </Container>
