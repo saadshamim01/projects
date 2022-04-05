05b9ba0ad6760195c8ac4155848aed77a15376f6
Saad Shamim <saadshamim01@gmail.com>
2022-04-06 00:46:03 +0400

    Added local storage

--- django/ecommerce/frontend/src/store.js
+++ django/ecommerce/frontend/src/store.js
@@ -1,14 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
-import { productListReducer } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
+
+  cart: cartReducer,
 });
 
-const initialState = {};
+const carItmesFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: carItmesFromStorage },
+};
 
 const middleware = [thunk];
 
