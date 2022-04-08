c5d148454824f6460df1cb478ce65719c9a3fa95
Saad Shamim <saadshamim01@gmail.com>
2022-04-09 03:27:14 +0400

    Revert "Updated Header and added more components

--- react/amazon-clone/src/App.js
+++ react/amazon-clone/src/App.js
@@ -3,11 +3,19 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { auth } from "./firebase";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect } from "react";
 import { useStateValue } from "./StateProvider";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51KTUclKM0nmL4AcCIV1cNPzRkMq4aEsUudUdefDQ8jPIAbCljpPaM85WoONYmBViwY61Wp87JoOe7XYpKgOq7c9M000I74uEsq"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -37,7 +45,17 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/login" element={[<Login />]} />
+
           <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route
+            path="/payment"
+            element={[
+              <Header />,
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>,
+            ]}
+          />
           <Route path="/" element={[<Header />, <Home />]} />
         </Routes>
       </div>
